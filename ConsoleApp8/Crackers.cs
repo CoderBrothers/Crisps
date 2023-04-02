8f9e156ea7d6ab67852ad394c4a4326c0d7af89d
CoderBrothers <121336523+CoderBrothers@users.noreply.github.com>
2023-04-02 20:21:16 +0300

    Add project files.

---
+++ ConsoleApp8/Crackers.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Crackers
+    {
+        #region PrivateFields
+        private double _weight;
+        private string _taste;
+        private double _price;
+        private string _title;
+        private static int _counter;
+        private int _id;
+        #endregion
+        #region Properties
+        public string Title
+        {
+            get => _title;
+            set
+            {
+                if (value.Trim() != String.Empty) _title = value;
+            }
+        }
+        public string Taste
+        {
+            get => _taste;
+            set
+            {
+                if (value.Trim() != String.Empty) _taste = value;
+            }
+        }
+        public double Weight
+        {
+            get => _weight;
+            set
+            {
+                if (value >= 50 && value < 300) _weight = value;
+            }
+        }
+        public double Price
+        {
+            get => _price;
+            set
+            {
+                if (value > 0 && value < 120) _price = value;
+            }
+        }
+        public static int Counter => _counter;
+        #endregion
+        #region Methods
+        public void Buy() => Console.WriteLine($"Я купил сухарики {Title}.");
+        public void About() => Console.WriteLine($"Я купил сухарики за {Price}грн :\nВес: {Weight}гр\nВкус: {Taste}");
+        public static void GetCount() => Console.WriteLine($"Создано {Counter} уп. сухариков");
+        #endregion
+        #region Constructors
+        public Crackers() : this("Flint") { }
+        public Crackers(string title) : this(title, 150) { }
+        public Crackers(string title, double weight) : this(title, weight, 45) { }
+        public Crackers(string title, double weight, double price) : this(title, weight, price, "Сметана и зелень") { }
+        public Crackers(string title, double weight, double price, string taste)
+        {
+            Title = !String.IsNullOrWhiteSpace(title) ? title : "Flint";
+            Weight = weight;
+            Price = price;
+            Taste = !String.IsNullOrWhiteSpace(taste) ? taste : "Сметана и зелень";
+            _counter++;
+        }
+
+
+        #endregion
+    }
+   
+
+}
---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,10 @@
+﻿using ConsoleApp8;
+
+ConsoleApp8.Crackers crackers = new ConsoleApp8.Crackers();
+crackers.Buy();
+crackers.About();
+ConsoleApp8.Crackers crackers1 = new ConsoleApp8.Crackers("Гренки", 255, 49, "Бекон");
+crackers1.Buy();
+crackers1.About();
+Console.WriteLine(ConsoleApp8.Crackers.Counter);
+ConsoleApp8.Crackers.GetCount();
